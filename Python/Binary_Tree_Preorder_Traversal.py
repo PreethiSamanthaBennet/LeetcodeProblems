9ceee0a1b443ad675ccf8722b132153b3675ca6a
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-01-21 10:38:09 +0530

    Create Binary_Tree_Preorder_Traversal.py

---
+++ Python/Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return res
