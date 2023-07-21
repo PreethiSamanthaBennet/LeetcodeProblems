5e54a2b1c7add64ca96dbb171a0d5aca7ef75674
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-07-21 17:41:24 +0530

    Create Valid_perfect_square.py

---
+++ Python/Valid_perfect_square.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        for i in range(1,num+1):
+            if i * i == num:
+                return True
+
+            if i * i > num:
+                return False
+
+        l, r = 1, num
+
+        while l <= r:
+            mid = (l+r)//2
+
+            if mid * mid > num:
+                r = mid - 1
+
+            elif mid * mid < num:
+                l = mid + 1
+
+            else:
+                return True
+
+        return False
