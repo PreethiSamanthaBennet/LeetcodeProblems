1454fe3616a2fcfaf4ed0758f41bb1eb7e856e02
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-07-25 14:14:54 +0530

    Create Peak_index_in_mountain_array.py

---
+++ Python/Peak_index_in_mountain_array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left, right = 0, len(arr) - 1
+
+        while left < right:
+            mid = (left + right)//2
+
+            if arr[mid] < arr[mid+1]:
+                left = mid+1
+
+            else:
+                right = mid
+
+        return left
