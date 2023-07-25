088163b0f6a3fe2db5873dcbb46b913990fab153
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-07-25 18:11:47 +0530

    Create Convert_the_temperature.py

---
+++ Python/Convert_the_temperature.py
@@ -0,0 +1,6 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        Kelvin = celsius + 273.15
+
+        Fahrenheit = celsius * 1.80 + 32.00
+        return Kelvin, Fahrenheit
