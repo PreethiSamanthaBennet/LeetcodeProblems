eaad72264b43be9fbf2c38c5cf7ad73d439ac3ab
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-08-01 14:48:11 +0530

    Create Combinations.py

---
+++ Python/Combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, comb):
+            if len(comb) == k:
+                res.append(comb.copy())
+                return
+
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i+1, comb)
+                comb.pop()
+
+        backtrack(1, [])
+        return res
